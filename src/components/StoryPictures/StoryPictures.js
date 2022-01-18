ccb6c9bb0cd2d0990152bad40625febf36d7aa6a
Marko Milanovic <milanovicmarko992@gmail.com>
2022-01-18 13:55:00 +0100

    moved images to public folder

--- src/components/StoryPictures/StoryPictures.js
+++ src/components/StoryPictures/StoryPictures.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './StoryPictures.scss';
-import Story1 from '../../assets/images/story-1.jpeg';
-import Story2 from '../../assets/images/story-2.jpeg';
 
 const StoryPictures = () => {
   return (
     <div className="StoryPictures">
-      <img src={Story1} alt="Couple with new house" className="story-img--1" />
-      <img src={Story2} alt="New house" className="story-img--2" />
+      <img
+        src="images/story-1.jpeg"
+        alt="Couple with new house"
+        className="story-img--1"
+      />
+      <img src="images/story-2.jpeg" alt="New house" className="story-img--2" />
     </div>
   );
 };
