7e7a82efc82a7ecb38c4b1cb8a4fbd14b6e3d9fd
Marko Milanovic <milanovicmarko992@gmail.com>
2022-01-18 12:03:32 +0100

    made custom button and finished story content

--- src/components/StoryContent/StoryContent.js
+++ src/components/StoryContent/StoryContent.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './StoryContent.scss';
+import Button from '../Button/Button';
 
 const StoryContent = () => {
   return (
     <div className="StoryContent">
-      <h1>StoryContent</h1>
+      <h3 className="heading-3 mb-sm">Happy Customers</h3>
+      <h2 className="heading-2 heading-2--dark mb-md">
+        &ldquo;The best decision of our lives&rdquo;
+      </h2>
+      <p className="story-text mb-lg">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+        harum volupta!
+      </p>
+      <Button text="Find your own home" />
     </div>
   );
 };
