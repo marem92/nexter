d1d2adfdc01843ca4e9d4c1e9bf13b665bce60fd
Marko Milanovic <milanovicmarko992@gmail.com>
2022-01-18 13:25:06 +0100

    finished story pictures section

--- src/components/StoryPictures/StoryPictures.js
+++ src/components/StoryPictures/StoryPictures.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './StoryPictures.scss';
+import Story1 from '../../assets/images/story-1.jpeg';
+import Story2 from '../../assets/images/story-2.jpeg';
 
 const StoryPictures = () => {
   return (
     <div className="StoryPictures">
-      <h1>StoryPictures</h1>
+      <img src={Story1} alt="Couple with new house" className="story-img--1" />
+      <img src={Story2} alt="New house" className="story-img--2" />
     </div>
   );
 };
