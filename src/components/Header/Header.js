488380925986d1384f5d95ace0b440378c459b4c
Marko Milanovic <milanovicmarko992@gmail.com>
2022-01-25 15:56:46 +0100

    finished header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Header.scss';
+import Button from '../Button/Button';
 
 const Header = () => {
   return (
-    <div className="Header">
-      <h1>Header</h1>
+    <div className="header">
+      <img src="images/logo.png" alt="Nexter logo" className="header__logo" />
+      <h3 className="heading-3">Your own home:</h3>
+      <h1 className="heading-1">The ultimate personal freedom</h1>
+      <Button text="View our properties" />
+      <div className="header__seenon-text">Seen on</div>
+      <div className="header__seenon-logos">
+        <img src="images/logo-bbc.png" alt="Seen on logo 1" />
+        <img src="images/logo-forbes.png" alt="Seen on logo 2" />
+        <img src="images/logo-techcrunch.png" alt="Seen on logo 3" />
+        <img src="images/logo-bi.png" alt="Seen on logo 4" />
+      </div>
     </div>
   );
 };
